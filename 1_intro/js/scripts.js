efce36f2606568814c94ee119699c14cb063321f
Alessio Vietri <info@alessiovietri.it>
2022-12-16 11:24:45 +0100

    Completato LC intro

---
+++ 1_intro/js/scripts.js
@@ -0,0 +1,23 @@
+// Commento singola riga
+
+/*
+    Commento
+    multi
+    -
+    riga
+*/
+
+document.writeln('Hello World!'); // Serve a stampare nell'HTML (come ultima riga) la scritta Hello World!
+
+
+alert('Hello World!'); /* Serve a mostrare un popup all'utente contenente la scritta Hello World! */
+console.log('Hello World!');
+
+// alert('Hello World 2!');
+// alert('Hello World 3!');
+
+document.getElementById('mio_id').innerHTML = 'Super Hello World!'; // Serve per cambiare contenuto ad un tag HTML
+
+console.log('Fin qui tutto ok');
+
+document.getElementById('test').innerHTML = 'Super Hello World!';
